0b7443678d948c4626ccf3f4c8421a8b3f3d1012
Matej Plavevski <matej.plavevski+github@gmail.com>
2024-10-09 23:34:13 +0200

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,6 @@ import { svelte } from '@sveltejs/vite-plugin-svelte'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/stellar-tipbot/",
+  base: "/stellar-tipbot-web/",
   plugins: [svelte()],
 })
